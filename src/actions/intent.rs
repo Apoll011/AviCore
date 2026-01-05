50e9fd1077f5e72c3b820fc6290c513cd8df9885
Apoll0_11 <tiagorobotik@gmail.com>
2026-01-05 18:13:38 +0000

    Separated parsing functionality and refactored IntentAction

--- src/actions/intent.rs
+++ src/actions/intent.rs
@@ -15,6 +15,56 @@ pub struct IntentAction {
 
 pub struct IntentConfig {}
 
+impl IntentAction {
+    pub async fn parse_as_reply(&self, text: &str) -> bool {
+        let skill_manager = Arc::clone(&self.skill_manager);
+
+        match runtime().reply_manager.process_text(text).await {
+            Ok(replay) => {
+                let mut mg = skill_manager.lock().await;
+                if let Err(e) = mg.run_skill_function(
+                    &replay.pending_reply.skill_request,
+                    &replay.pending_reply.handler,
+                    vec![replay.parsed_output],
+                ) {
+                    println!("Error executing replay: {}", e);
+                }
+                true
+            }
+            Err(e) => {
+                if !e.is_empty() {
+                    speak!(&e);
+                }
+                false
+            }
+        }
+    }
+
+    pub async fn parse_as_intent(&self, text: &str) -> bool {
+        let api = Arc::clone(&self.api);
+        let skill_manager = Arc::clone(&self.skill_manager);
+
+        let maybe_intent = match api.lock().await.intent(text).await {
+            Ok(intent) => Some(intent),
+            Err(e) => {
+                println!("Failed to parse intent: {}", e);
+                None
+            }
+        };
+
+        if let Some(intent) = maybe_intent {
+            let mut mg = skill_manager.lock().await;
+
+            if let Err(e) = mg.run_intent(intent) {
+                println!("Error executing intent: {}", e);
+            } else {
+                return true;
+            }
+        }
+        false
+    }
+}
+
 impl Action for IntentAction {
     type Config = IntentConfig;
 
@@ -27,56 +77,29 @@ impl Action for IntentAction {
     }
 
     async fn register(&mut self) {
+        let device = Arc::clone(&self.device);
         let api = Arc::clone(&self.api);
         let skill_manager = Arc::clone(&self.skill_manager);
 
         match self
             .device
             .subscribe_async("intent/execute/text", move |_from, _topic, data| {
+                let device = Arc::clone(&device);
                 let api = Arc::clone(&api);
                 let skill_manager = Arc::clone(&skill_manager);
 
                 async move {
                     let msg = String::from_utf8_lossy(&data);
                     let text = msg.trim();
-                    let mut processed = false;
 
-                    match runtime().reply_manager.process_text(text).await {
-                        Ok(replay) => {
-                            let mut mg = skill_manager.lock().await;
-                            if let Err(e) = mg.run_skill_function(
-                                &replay.pending_reply.skill_request,
-                                &replay.pending_reply.handler,
-                                vec![replay.parsed_output],
-                            ) {
-                                println!("Error executing replay: {}", e);
-                            }
-                            processed = true;
-                        }
-                        Err(e) => {
-                            if !e.is_empty() {
-                                speak!(&e);
-                            }
-                        }
-                    }
-
-                    if processed {
-                        return;
-                    }
-
-                    let maybe_intent = match api.lock().await.intent(&msg).await {
-                        Ok(intent) => Some(intent),
-                        Err(e) => {
-                            println!("Failed to parse intent: {}", e);
-                            None
-                        }
+                    let intent_action = IntentAction {
+                        device: Arc::clone(&device),
+                        api,
+                        skill_manager,
                     };
 
-                    if let Some(intent) = maybe_intent {
-                        let mut mg = skill_manager.lock().await;
-                        if let Err(e) = mg.run_intent(intent) {
-                            println!("Error executing intent: {}", e);
-                        }
+                    if !intent_action.parse_as_reply(text).await {
+                        let _ = intent_action.parse_as_intent(text).await;
                     }
                 }
             })
