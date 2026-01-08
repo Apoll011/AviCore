9d5c3858008811ddd48544a51472e3e83a5bb8dd
Apoll0_11 <tiagorobotik@gmail.com>
2026-01-08 19:28:55 +0000

    Added some filesystem dsl functions

---
+++ src/skills/dsl/fs.rs
@@ -0,0 +1,68 @@
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::Write;
+use dyon::{Dfn, Module, Runtime};
+use std::result::Result;
+
+use std::sync::Arc;
+use log::warn;
+
+pub fn add_functions(module: &mut Module) {
+    use dyon::Type::*;
+
+    module.ns("fs");
+    module.add(
+        Arc::new("read".into()),
+        read,
+        Dfn::nl(vec![Str], Option(Box::from(Str))),
+    );
+    module.add(
+        Arc::new("write".into()),
+        write,
+        Dfn::nl(vec![Str, Str], Void),
+    );
+    module.add(
+        Arc::new("append".into()),
+        write,
+        Dfn::nl(vec![Str, Str], Void),
+    );
+}
+
+dyon_fn! { fn read(path: String) -> Option<String> {
+    match fs::read_to_string(path) {
+        Ok(file) => Some(file),
+        Err(e) => {
+            warn!("Skill: Error reading the file content: {}", e);
+            None
+        },
+    }
+}}
+
+pub fn write(_rt: &mut Runtime) -> Result<(), String> {
+    let content: String = _rt.pop()?;
+    let path: String = _rt.pop()?;
+
+    let mut file = OpenOptions::new()
+        .create(true)
+        .open(path)
+        .expect("Could not open log file");
+
+    writeln!(file, "{}", content).expect("Could not write to log file");
+
+    Ok(())
+}
+
+pub fn append(_rt: &mut Runtime) -> Result<(), String> {
+    let content: String = _rt.pop()?;
+    let path: String = _rt.pop()?;
+
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path)
+        .expect("Could not open log file");
+
+    writeln!(file, "{}", content).expect("Could not write to log file");
+
+    Ok(())
+}
