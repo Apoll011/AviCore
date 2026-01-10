7ca5454dd7fd3881cf8213dc4cadbecbfcb2f7c3
Apoll0_11 <tiagorobotik@gmail.com>
2026-01-10 00:00:52 +0000

    Fixed on_reply handler

--- src/skills/avi_script/module/dialogue.rs
+++ src/skills/avi_script/module/dialogue.rs
@@ -1,49 +1,50 @@
+use log::error;
 use crate::ctx::runtime;
-use crate::dialogue::reply::RequestReply;
-use crate::dialogue::response::BoolValidator;
+use crate::dialogue::reply::{RequestReply, ValidatorErasure};
 use crate::dialogue::utils::speak;
 use crate::user::user_name;
 use crate::{get_ctx, register_skill_func, rt_spawn, speak};
 use rhai::module_resolvers::StaticModuleResolver;
 use rhai::{Dynamic, FuncRegistration, Module};
+use crate::dialogue::response::{AnyValidator, BoolValidator, ListOrNoneValidator, MappedValidator, OptionalValidator};
 
 pub fn add(resolver: &mut StaticModuleResolver) {
     let mut module = Module::new();
 
     FuncRegistration::new("any_validator")
-        .set_into_module(&mut module, crate::dialogue::response::AnyValidator::new);
+        .set_into_module(&mut module, AnyValidator::new);
     FuncRegistration::new("list_or_none_validator").set_into_module(
         &mut module,
-        crate::dialogue::response::ListOrNoneValidator::new,
+        ListOrNoneValidator::new,
     );
     FuncRegistration::new("optional_validator").set_into_module(
         &mut module,
-        crate::dialogue::response::OptionalValidator::new,
+        OptionalValidator::new,
     );
     FuncRegistration::new("bool_validator").set_into_module(&mut module, BoolValidator::new);
     FuncRegistration::new("mapped_validator_string").set_into_module(
         &mut module,
-        crate::dialogue::response::MappedValidator::<String>::new,
+        MappedValidator::<String>::new,
     );
     FuncRegistration::new("mapped_validator_i32").set_into_module(
         &mut module,
-        crate::dialogue::response::MappedValidator::<i32>::new,
+        MappedValidator::<i32>::new,
     );
     FuncRegistration::new("mapped_validator_i64").set_into_module(
         &mut module,
-        crate::dialogue::response::MappedValidator::<i64>::new,
+        MappedValidator::<i64>::new,
     );
     FuncRegistration::new("mapped_validator_f32").set_into_module(
         &mut module,
-        crate::dialogue::response::MappedValidator::<f32>::new,
+        MappedValidator::<f32>::new,
     );
     FuncRegistration::new("mapped_validator_f64").set_into_module(
         &mut module,
-        crate::dialogue::response::MappedValidator::<f64>::new,
+        MappedValidator::<f64>::new,
     );
     FuncRegistration::new("mapped_validator_bool").set_into_module(
         &mut module,
-        crate::dialogue::response::MappedValidator::<bool>::new,
+        MappedValidator::<bool>::new,
     );
     FuncRegistration::new("say").set_into_module(&mut module, say);
     FuncRegistration::new("say_once").set_into_module(&mut module, say_once);
@@ -97,12 +98,37 @@ fn repeat() {
 }
 
 fn handle_on_reply(handler: String, validator: Dynamic, skill_name: String) {
+    let validator_erased: Box<dyn ValidatorErasure> = if validator.is::<BoolValidator>() {
+        Box::new(validator.cast::<BoolValidator>())
+    } else if validator.is::<AnyValidator>() {
+        Box::new(validator.cast::<AnyValidator>())
+    } else if validator.is::<ListOrNoneValidator>() {
+        Box::new(validator.cast::<ListOrNoneValidator>())
+    } else if validator.is::<OptionalValidator>() {
+        Box::new(validator.cast::<OptionalValidator>())
+    } else if validator.is::<MappedValidator<String>>() {
+        Box::new(validator.cast::<MappedValidator<String>>())
+    } else if validator.is::<MappedValidator<i32>>() {
+        Box::new(validator.cast::<MappedValidator<i32>>())
+    } else if validator.is::<MappedValidator<i64>>() {
+        Box::new(validator.cast::<MappedValidator<i64>>())
+    } else if validator.is::<MappedValidator<f32>>() {
+        Box::new(validator.cast::<MappedValidator<f32>>())
+    } else if validator.is::<MappedValidator<f64>>() {
+        Box::new(validator.cast::<MappedValidator<f64>>())
+    } else if validator.is::<MappedValidator<bool>>() {
+        Box::new(validator.cast::<MappedValidator<bool>>())
+    } else {
+        error!("Unknown validator type: {}", validator.type_name());
+        return;
+    };
+
     rt_spawn! {
         if let Ok(c) = runtime() { c.reply_manager
         .set_reply(RequestReply {
             skill_request: skill_name,
             handler,
-            validator: validator.try_cast().unwrap(),
+            validator: validator_erased,
         })
         .await };
     }
